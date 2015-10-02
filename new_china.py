aba1e7fc5c63d7ab770e5d052782cb218af77662
Steven Buss <steven.buss@gmail.com>
2015-10-01 21:39:21 -0700

    Use different sources

--- new_china.py
+++ new_china.py
@@ -2,9 +2,13 @@ from dump import get_writer
 from fetch import yield_contents
 
 urls = [
-    ('https://quizlet.com/47828476/reading-into-a-new-china-chapter-1-flash-cards/', ';', 'lesson_1'),  # nopep8
-    ('https://quizlet.com/53308491/reading-into-a-new-china-1-l2-flash-cards/', '\n', 'lesson_2'),  # nopep8
-    ('https://quizlet.com/53308523/reading-into-a-new-china-1-l3-flash-cards/', '\n', 'lesson_3'),  # nopep8
+    ('https://quizlet.com/12455900/1-l1-all-flash-cards/', ':', 'lesson_1')  # nopep8
+    ('https://quizlet.com/12455865/1-l2-all-flash-cards/', ':', 'lesson_2'),  # nopep8
+    ('https://quizlet.com/12588906/1-l3-all-flash-cards/', '\n', 'lesson_3'),  # nopep8
+    ('https://quizlet.com/12589825/1-l4-all-flash-cards/', '\n', 'lesson_4'),  # nopep8
+    ('https://quizlet.com/12589998/1-l5-all-flash-cards/', '\n', 'lesson_5'),  # nopep8
+    ('https://quizlet.com/12590420/1-l6-all-flash-cards/', '\n', 'lesson_6'),  # nopep8
+    ('https://quizlet.com/12591711/1-l7-all-flash-cards/', '\n', 'lesson_7'),  # nopep8
 ]
 
 
