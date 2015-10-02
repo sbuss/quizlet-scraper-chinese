988ae937ced7668efc137e24a0e25cafaf5c9379
Steven Buss <steven.buss@gmail.com>
2015-10-01 21:22:35 -0700

    add tags to new_china

--- new_china.py
+++ new_china.py
@@ -2,14 +2,15 @@ from dump import get_writer
 from fetch import yield_contents
 
 urls = [
-    ('https://quizlet.com/47828476/reading-into-a-new-china-chapter-1-flash-cards/', ';'),  # nopep8
-    ('https://quizlet.com/53308491/reading-into-a-new-china-1-l2-flash-cards/', '\n'),  # nopep8
-    ('https://quizlet.com/53308523/reading-into-a-new-china-1-l3-flash-cards/', '\n'),  # nopep8
+    ('https://quizlet.com/47828476/reading-into-a-new-china-chapter-1-flash-cards/', ';', 'lesson_1'),  # nopep8
+    ('https://quizlet.com/53308491/reading-into-a-new-china-1-l2-flash-cards/', '\n', 'lesson_2'),  # nopep8
+    ('https://quizlet.com/53308523/reading-into-a-new-china-1-l3-flash-cards/', '\n', 'lesson_3'),  # nopep8
 ]
 
 
 if __name__ == "__main__":
     writer = get_writer('output/new_china.csv')
-    for url, sep in urls:
+    for url, sep, tags in urls:
         for content_dict in yield_contents(url, sep):
+            content_dict['tags'] = tags
             writer.writerow(content_dict)
